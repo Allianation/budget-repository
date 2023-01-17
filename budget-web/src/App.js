d04606ab2e0861164bf60a0273ed476f6241fb5a
Sergio <salliana@technisys.com>
2023-01-17 16:24:56 -0300

    Implemented redux

--- budget-web/src/App.js
+++ budget-web/src/App.js
@@ -7,30 +7,21 @@ import EntryLines from "./components/EntryLines";
 import MainHeader from "./components/MainHeader";
 import NewEntryForm from "./components/NewEntryForm";
 import ModalEdit from "./components/ModalEdit";
+import { useSelector } from "react-redux";
 
 function App() {
-  const [entries, setEntries] = useState(initialEntries);
-  const [description, setDescription] = useState("");
-  const [value, setValue] = useState("");
-  const [isExpense, setIsExpense] = useState(true);
-  const [isOpen, setIsOpen] = useState(false);
-  const [entryId, setEntryId] = useState("");
   const [totalIncomes, setTotalIncomes] = useState(0);
   const [totalExpenses, setTotalExpenses] = useState(0);
   const [total, setTotal] = useState(0);
+  const [entry, setEntry] = useState();
+  const entries = useSelector((state) => state.entries);
+  const { isOpen, id } = useSelector((state) => state.modals);
 
   useEffect(() => {
-    if (!isOpen && entryId) {
-      const index = entries.findIndex((entry) => entry.id === entryId);
-      const newEntries = [...entries];
-      newEntries[index].description = description;
-      newEntries[index].value = value;
-      newEntries[index].isExpense = isExpense;
-      setEntries(newEntries);
-      resetEntry();
-    }
+    const index = entries.findIndex((entry) => entry.id === id);
+    setEntry(entries[index]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isOpen]);
+  }, [isOpen, id]);
 
   useEffect(() => {
     let totalIncomes = 0;
@@ -48,46 +39,6 @@ function App() {
     setTotalIncomes(totalIncomes);
   }, [entries]);
 
-  // const deleteEntry = (id) => {}
-  function deleteEntry(id) {
-    const result = entries.filter((entry) => entry.id !== id);
-    //console.log("Entries", entries);
-    //console.log("Result", result);
-    setEntries(result);
-  }
-
-  function addEntry() {
-    const result = entries.concat({
-      id: entries.length + 1,
-      description: description,
-      value: value,
-      isExpense: isExpense,
-    });
-    //console.log("Entries", entries);
-    //console.log("Result", result);
-    setEntries(result);
-    resetEntry();
-  }
-
-  function editEntry(id) {
-    if (id) {
-      //console.log("Edit entry with id: ", id);
-      const index = entries.findIndex((entry) => entry.id === id);
-      const entry = entries[index];
-      setEntryId(id);
-      setDescription(entry.description);
-      setValue(entry.value);
-      setIsExpense(entry.isExpense);
-      setIsOpen(true);
-    }
-  }
-
-  function resetEntry() {
-    setDescription("");
-    setValue("");
-    setIsExpense(true);
-  }
-
   return (
     <Container>
       <MainHeader title="Budget"></MainHeader>
@@ -103,62 +54,14 @@ function App() {
       ></DisplayBalances>
 
       <MainHeader title="History" type="h3"></MainHeader>
-      <EntryLines
-        entries={entries}
-        deleteEntry={deleteEntry}
-        editEntry={editEntry}
-      ></EntryLines>
+      <EntryLines entries={entries}></EntryLines>
 
       <MainHeader title="Add new transaction" type="h3"></MainHeader>
-      <NewEntryForm
-        addEntry={addEntry}
-        description={description}
-        value={value}
-        isExpense={isExpense}
-        setDescription={setDescription}
-        setValue={setValue}
-        setIsExpense={setIsExpense}
-      ></NewEntryForm>
-      <ModalEdit
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        description={description}
-        value={value}
-        isExpense={isExpense}
-        setDescription={setDescription}
-        setValue={setValue}
-        setIsExpense={setIsExpense}
-      ></ModalEdit>
+      <NewEntryForm></NewEntryForm>
+      <ModalEdit isOpen={isOpen} {...entry}></ModalEdit>
       <br></br>
     </Container>
   );
 }
 
 export default App;
-
-var initialEntries = [
-  {
-    id: 1,
-    description: "Work income",
-    value: 1000.0,
-    isExpense: false,
-  },
-  {
-    id: 2,
-    description: "Water bill",
-    value: 20.0,
-    isExpense: true,
-  },
-  {
-    id: 3,
-    description: "Rent",
-    value: 300.0,
-    isExpense: true,
-  },
-  {
-    id: 4,
-    description: "Power bill",
-    value: 50.0,
-    isExpense: true,
-  },
-];
--- budget-web/src/components/EntryLine.js
+++ budget-web/src/components/EntryLine.js
@@ -1,8 +1,12 @@
 import React, { Fragment } from "react";
 import { Grid, Icon, Segment } from "semantic-ui-react";
+import { useDispatch } from "react-redux";
+import { removeEntryRedux } from "../actions/entries.actions";
+import { openEditModal } from "../actions/modals.actions";
 
-//function EntryLine({ entry: { description, value, isExpense = false } }) {
-function EntryLine({ id, description, value, isExpense = false, deleteEntry, editEntry }) {
+function EntryLine({ id, description, value, isExpense = false }) {
+  const dispatch = useDispatch();
+
   return (
     <Fragment>
       <Segment color={isExpense ? "red" : "green"}>
@@ -15,11 +19,15 @@ function EntryLine({ id, description, value, isExpense = false, deleteEntry, edi
               {value}
             </Grid.Column>
             <Grid.Column width={3}>
-              <Icon name="edit" bordered onClick={() => editEntry(id)}></Icon>
+              <Icon
+                name="edit"
+                bordered
+                onClick={() => dispatch(openEditModal(id))}
+              ></Icon>
               <Icon
                 name="trash"
                 bordered
-                onClick={() => deleteEntry(id)}
+                onClick={() => dispatch(removeEntryRedux(id))}
               ></Icon>
             </Grid.Column>
           </Grid.Row>
